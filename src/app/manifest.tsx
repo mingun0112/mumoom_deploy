b00d7759cb0e9a3cc58ebc219d7c18d71eb2c845
정민균 <mingun0112@naver.com>
2025-05-07 02:19:10 +0900

    add logo

--- src/app/manifest.tsx
+++ src/app/manifest.tsx
@@ -2,8 +2,8 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Next.js PWA',
-        short_name: 'NextPWA',
+        name: 'UnoffiMap',
+        short_name: 'UnoffiMap',
         description: 'A Progressive Web App built with Next.js',
         start_url: '/',
         display: 'standalone',
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/images.png',
+                src: '/logo.jpg',
                 sizes: '192x192',
-                type: 'image/png',
+                type: 'image/jpg',
             },
             {
-                src: '/images.png',
+                src: '/logo.jpg',
                 sizes: '512x512',
-                type: 'image/png',
+                type: 'image/jpg',
             },
         ],
     }
