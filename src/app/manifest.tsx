02072408440dc55953ed79035fab79627863be60
정민균 <mingun0112@naver.com>
2025-05-07 02:35:20 +0900

    logo to png

--- src/app/manifest.tsx
+++ src/app/manifest.tsx
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/logo.jpg',
+                src: '/logo.png',
                 sizes: '192x192',
-                type: 'image/jpg',
+                type: 'image/png',
             },
             {
-                src: '/logo.jpg',
+                src: '/logo.png',
                 sizes: '512x512',
-                type: 'image/jpg',
+                type: 'image/png',
             },
         ],
     }
